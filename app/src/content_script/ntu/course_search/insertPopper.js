688677c714ae6ef80ad18e099d96576a408c799a
cin-yi-wei <m95162027@gmail.com>
2021-02-26 22:54:48 +0800

    let it bigger

--- app/src/content_script/ntu/course_search/insertPopper.js
+++ app/src/content_script/ntu/course_search/insertPopper.js
@@ -1,7 +1,7 @@
 function insertPopper(){
   let popper = document.createElement('div');
   popper.className = "popper";
-  popper.style = "position: absolute;background: #F5F5F5;color: black;width: 400px;height: 200px;border-radius: 20px;box-shadow: 0 0 2px rgba(0,0,0,0.5);padding: 10px;text-align: center;";
+  popper.style = "position: absolute;background: #F5F5F5;color: black;width: 600px;height: 300px;border-radius: 20px;box-shadow: 0 0 2px rgba(0,0,0,0.5);padding: 10px;text-align: center;";
   popper.style.cssText +="margin-right: 10px;";
   popper.setAttribute("hidden","")
   let popper_arrow = document.createElement('div');
